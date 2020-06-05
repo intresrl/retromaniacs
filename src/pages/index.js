4477293c7358e01e31a0f4c975e544756e51ce93
Francesco Sacchi <sacchi@intre.it>
2020-06-05 18:40:31 +0200

    Added new cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {useStaticQuery, graphql, Link} from "gatsby"
+import {useStaticQuery, graphql} from "gatsby"
 import Card from "../components/card";
 import "../../static/style.css"
 
