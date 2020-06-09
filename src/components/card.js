468dbce52cd2aa17da014ad9184733241865febc
Francesco Sacchi <sacchi@intre.it>
2020-06-09 17:55:24 +0200

    Added selection for stage one

--- src/components/card.js
+++ src/components/card.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import {Link} from "gatsby";
 
-const Card = ({card}) => {
-    return <div style={{margin: '5px', border: "1px solid black", padding: "5px", maxWidth: "300px"}}>
-        <Link to={`/cards/${card.id}`}>
+const Card = ({card, onClick}) => {
+    return <div  onClick={onClick} style={{margin: '5px', border: "1px solid black", padding: "5px", maxWidth: "300px", minWidth: "300px"}}>
         <h1>{card.name}</h1>
         <h2>{card.stage.join(", ")}</h2>
         <p>{`Tempo: da ${card.timeNeeded.min} a ${card.timeNeeded.max} minuti`}</p>
         <p>{`Scopo ${card.needSolved}`}</p>
-    </Link>
     </div>
 }
 
