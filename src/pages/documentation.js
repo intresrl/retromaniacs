164cb0a2c9e95967adfad698b8999ff46a63a6aa
michele.nava <michele.nava@consultant.rgigroup.com>
2020-06-09 18:00:29 +0200

    Added page with rules (draft)

--- src/pages/documentation.js
+++ src/pages/documentation.js
@@ -1,9 +1,24 @@
 import React from "react"
 
 const Documentation = () => {
-  return (<div>
-    Questa è la doc
-  </div>)
+    return (<div>
+        <h1>REGOLAMENTO</h1>
+        <h2>Preparazione della retrospettiva:</h2>
+        <ul>
+            <li>L'utente sceglie il tempo totale della retrospettiva e in base a questo sceglie un sottoinsieme di
+                carte.
+            </li>
+            <li>Si scelgono le carte per ognuna delle 5 aree:</li>
+            <ul>
+                <li>Set the stage</li>
+                <li>Gather data</li>
+                <li>Generate insight</li>
+                <li>Decide what to do</li>
+                <li>Close the retrospective</li>
+            </ul>
+            <li>È possible segnarsi delle note nella sezione apposita.</li>
+        </ul>
+    </div>)
 }
 
 export default Documentation;
