edfa06ade0acd69dc7db030a4b781b0a26b1ee29
Francesco Sacchi <sacchi@intre.it>
2020-06-09 22:14:36 +0200

    Added other sections

--- src/pages/plan.js
+++ src/pages/plan.js
@@ -27,6 +27,10 @@ export default function Home() {
   `)
 
   const [card1, setCard1] = useState(null)
+  const [card2, setCard2] = useState(null)
+  const [card3, setCard3] = useState(null)
+  const [card4, setCard4] = useState(null)
+  const [card5, setCard5] = useState(null)
 
     return <div>
       <Helmet>
@@ -36,6 +40,30 @@ export default function Home() {
       <div style={{display: 'flex', padding: "5px", textTransform: "none", overflow: "auto"}}>
         {card1 ? <Card card={card1}/> :
           data.allCardsYaml.nodes.map(card => <Card card={card} onClick={() => setCard1(card)}/>)
+        }
+      </div>
+      <h2>Gather data</h2>
+      <div style={{display: 'flex', padding: "5px", textTransform: "none", overflow: "auto"}}>
+        {card2 ? <Card card={card2}/> :
+          data.allCardsYaml.nodes.map(card => <Card card={card} onClick={() => setCard2(card)}/>)
+        }
+      </div>
+      <h2>Generate insights</h2>
+      <div style={{display: 'flex', padding: "5px", textTransform: "none", overflow: "auto"}}>
+        {card3 ? <Card card={card3}/> :
+          data.allCardsYaml.nodes.map(card => <Card card={card} onClick={() => setCard3(card)}/>)
+        }
+      </div>
+      <h2>Decide what to do</h2>
+      <div style={{display: 'flex', padding: "5px", textTransform: "none", overflow: "auto"}}>
+        {card4 ? <Card card={card4}/> :
+          data.allCardsYaml.nodes.map(card => <Card card={card} onClick={() => setCard4(card)}/>)
+        }
+      </div>
+      <h2>Close the retrospective</h2>
+      <div style={{display: 'flex', padding: "5px", textTransform: "none", overflow: "auto"}}>
+        {card5 ? <Card card={card5}/> :
+          data.allCardsYaml.nodes.map(card => <Card card={card} onClick={() => setCard5(card)}/>)
         }
       </div>
     </div>
